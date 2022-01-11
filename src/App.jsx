5536c7d3f34a5434479b71041ae314975cd19a96
anderson <anderson.d.cardoso92@gmail.com>
2022-01-10 20:59:57 -0600

    debug call to crypto service

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,66 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css'
+import Coin from './Coin';
+
 
 function App() {
 
+  // const baseURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+      .then(res => {
+        console.log("response data -> ", res.data)
+        setCoins(res.data)
+      })
+      .catch((error) => console.log(error));
+
+    }, []);
+  
+  
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+  
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+    
+  console.log("coins -> ", coins);
+  console.log("=>", filteredCoins)
   return (
-    <div className="App">
-      <h1>Crypto Tracker</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Find a Currency</h1>
+        <form action="">
+          <input 
+            type="text" 
+            className="coin-input" 
+            onChange={handleChange}
+            placeholder='Search'
+            />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        )
+      })}
     </div>
-  )
+  );
 }
 
 export default App
